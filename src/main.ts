be454dda9aa1164c1f9d15ea7c112a9c4658ab7a
rosy-bama <bamarosy24@gmail.com>
2021-11-23 21:37:04 +0100

    Done setting up Prisma and migrating the models

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app.module';
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
