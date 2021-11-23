5816a2f06d6e1f7d589ba6727fb60077a0f97a28
rosy-bama <bamarosy24@gmail.com>
2021-11-23 21:54:51 +0100

    Done generating users and todos resources

---
+++ src/modules/todos.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { TodosService } from '../services/todos.service';
+import { TodosController } from '../controllers/todos.controller';
+
+@Module({
+  controllers: [TodosController],
+  providers: [TodosService]
+})
+export class TodosModule {}
